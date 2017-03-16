2e8e12ab069f082a0869b17d40c40a5729cb5863
snfazal <snfazal@gmail.com>
2017-03-15 22:21:48 -0400

    added factories

---
+++ public/js/controllers/home_controller.js
@@ -0,0 +1,11 @@
+// When only the name of the module is passed in,
+// the 'module' method returns the specified module.
+angular.module('media-app')
+    .controller('homeController', homeController);
+
+// This is the function definition for our controller.
+// Note that we capitalize it as it is used as a constructor function!
+function HomeController() {
+
+
+}
