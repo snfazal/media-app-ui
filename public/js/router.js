2aeb3261ba289efe39b4af9b6c382bbb9dc5ab28
snfazal <snfazal@gmail.com>
2017-03-15 22:32:04 -0400

    set up ui-router

---
+++ public/js/router.js
@@ -0,0 +1,18 @@
+angular.module('mediaAngularApp', ['ui.router']);
+  .config(MediaRouter)
+  .config(authInterceptor)
+
+function authInterceptor($httpProvider) {
+  $httpProvider.interceptors.push('AuthInterceptor')
+}
+
+function MediaRouter($stateProvider, $urlRouterProvider){
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('index', {
+        url: '/',
+        templateUrl: '/partials/home.html'
+      })
+}
