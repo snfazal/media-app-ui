f43faa147bf2a4a69d2e14b0715f54e532e02a65
snfazal <snfazal@gmail.com>
2017-03-19 16:44:51 -0400

    rewiring partial for show route

--- public/js/controllers/media_controller.js
+++ public/js/controllers/media_controller.js
@@ -23,10 +23,28 @@ function MediaController($http, $state, $scope){
   getMedia();
 
   function showMedia(media) {
+    console.log('media')
     self.nowShowing = media;
     $scope.$emit('nowShowing', self.nowShowing);
     $state.go('show_media');
   }
   self.showMedia = showMedia;
+  showMedia();
 
+  function editMedia(media){
+    $http.put(`${server}/media/${media.id}`, angular.toJson(media))
+    .then(function(res){
+      self.showEdit = false;
+    })
+    $state.reload()
+  }
+  self.editMedia = editMedia;
+
+  function deleteMedia(media){
+    $http.media(`${server}/media/${media.id}`)
+    .then(function(res){
+      $state.go('index')
+    })
+  }
+  self.deleteMedia = deleteMedia;
 }
